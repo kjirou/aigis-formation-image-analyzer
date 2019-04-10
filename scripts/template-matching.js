d86ed7f95d84ce299d3f40fd72c7bf40296afd30
kjirou <sorenariblog@gmail.com>
2019-04-10 21:07:44 +0900

    wip: SADをしてみたつもりだけど結果がダメってところ

--- scripts/template-matching.js
+++ scripts/template-matching.js
@@ -26,26 +26,78 @@ function convertPngToGrayscale(fromFilePath, toFilePath) {
   ;
 }
 
-function bitmapDataToMatrix(jimpImage) {
-  const {width, height, data} = jimpImage;
+//function bitmapDataToMatrix(jimpImage) {
+//  const {width, height, data} = jimpImage;
+//  const matrix = [];
+//  for (let y = 0; y < height; y++) {
+//    const row = [];
+//    for (let x = 0; x < width; x++) {
+//      row.push(data[y * width * 4 + x * 4]);
+//    }
+//    matrix.push(row);
+//  }
+//  return matrix;
+//}
+function jimpImageToMatrix(jimpImage) {
   const matrix = [];
-  for (let y = 0; y < height; y++) {
-    const row = [];
-    for (let x = 0; x < width; x++) {
-      row.push(data[y * width * 4 + x * 4]);
+  let currentY = -1;
+  let currentX = -1;
+  jimpImage.scan(0, 0, jimpImage.bitmap.width, jimpImage.bitmap.height, function(x, y, index) {
+    if (y === currentY + 1) {
+      matrix.push([]);
+      currentY = y;
+      currentX = -1;
+    } else if (y > currentY + 1) {
+      throw new Error('y がインクリメントされてない疑いがある');
     }
-    matrix.push(row);
-  }
+    if (x === currentX + 1) {
+      matrix[y].push(jimpImage.bitmap.data[index]);
+      currentX = x;
+    } else if (x > currentX + 1) {
+      throw new Error('x がインクリメントされてない疑いがある');
+    }
+  });
   return matrix;
 }
 
 /**
  * SAD (Sum of Absolute Differences)
  * https://en.wikipedia.org/wiki/Sum_of_absolute_differences
- * @param templateImageData {width, height, pixels}
- * @param searchImageData {width, height, pixels}
+ * @param subjectMatrix darkness[][]
+ * @param templateMatrix darkness[][]
  */
-function searchBySad(templateImageData, searchImageData) {
+function searchBySad(subjectMatrix, templateMatrix) {
+  const subjectHeight = subjectMatrix.length - 1;
+  const subjectWidth = subjectMatrix[0].length - 1;
+  const templateHeight = templateMatrix.length - 1;
+  const templateWidth = templateMatrix[0].length - 1;
+  const maxSubjectY = subjectHeight - templateHeight - 1;
+  const maxSubjectX = subjectWidth - templateWidth - 1;
+  const maxTemplateY = templateHeight - 1;
+  const maxTemplateX = templateWidth - 1;
+
+  const candidate = {
+    y: -1,
+    x: -1,
+    score: -1,
+  };
+  for (let sy = 0; sy <= maxSubjectY; sy++) {
+    for (let sx = 0; sx <= maxSubjectX; sx++) {
+      let score = 0;
+      for (let ty = 0; ty <= maxTemplateY; ty++) {
+        for (let tx = 0; tx <= maxTemplateX; tx++) {
+          score += Math.abs(subjectMatrix[sy][sx] - templateMatrix[ty][tx]);
+        }
+      }
+      if (candidate.score === -1 || score < candidate.score) {
+        candidate.y = sy;
+        candidate.x = sx;
+        candidate.score = score;
+      }
+    }
+  }
+
+  return candidate;
 }
 
 const conversions = [
@@ -83,23 +135,34 @@ const conversions = [
 fs.removeSync(TMP_ROOT);
 fs.ensureDirSync(TMP_BUILT_IMAGES_TEMPLATES_ROOT);
 
+let subjectImageMatrix;
+let templateImageMatrix;
+let searchedSubjectPixel;
+
 Promise.resolve()
   .then(() => {
     return Promise.all(conversions.map(([from, to]) => convertPngToGrayscale(from, to)));
+  })
+  .then(() => {
+    return jimp.read(path.join(TMP_BUILT_IMAGES_ROOT, 'formation-1--cropped.png'))
+      .then(image => {
+        console.log('Image =', image.bitmap.width, image.bitmap.height, image.bitmap.data.length);
+        subjectImageMatrix = jimpImageToMatrix(image);
+      })
+    ;
   })
   .then(() => {
     return jimp.read(path.join(TMP_BUILT_IMAGES_TEMPLATES_ROOT, 'tenma.png'))
       .then(image => {
         // RGBA で 1 ピクセル(画素)辺り 4 バイト割り当てられている
         // 実際はグレースケールしたので、[x, x, x, 255] になっていて x は同じ値
-        console.log(image.bitmap.width);
-        console.log(image.bitmap.height);
-        console.log(image.bitmap.data.length);
-        const matrix = bitmapDataToMatrix(image.bitmap);
-        console.log(matrix.length);
-        console.log(matrix[0].length);
-        console.log(matrix[matrix.length - 1].length);
+        console.log('Image =', image.bitmap.width, image.bitmap.height, image.bitmap.data.length);
+        templateImageMatrix = jimpImageToMatrix(image);
       })
     ;
+  })
+  .then(() => {
+    searchedSubjectPixel = searchBySad(subjectImageMatrix, templateImageMatrix);
+    console.log(searchedSubjectPixel);
   })
 ;
